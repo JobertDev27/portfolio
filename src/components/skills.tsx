6c3b0b689efed55429b90a907188fcaf669cfbec
JobertDev27 <jobertdev27@gmail.com>
2025-06-02 17:46:52 +0800

    added a storage for skills to easily add or change skills

---
+++ src/components/skills.tsx
@@ -0,0 +1,27 @@
+import {
+  Html,
+  Css,
+  Tailwind,
+  Javascript,
+  Typescript,
+  Reactjs,
+  Nodejs,
+  Express,
+  Postgres,
+  Git,
+  Figma,
+} from "./Svg";
+
+export const skills = [
+  { name: "HTML", image: <Html width="24px" height="24px" /> },
+  { name: "CSS", image: <Css width="24px" height="24px" /> },
+  { name: "Tailwind", image: <Tailwind width="24px" height="24px" /> },
+  { name: "JavaScript", image: <Javascript width="24px" height="24px" /> },
+  { name: "TypeScript", image: <Typescript width="24px" height="24px" /> },
+  { name: "ReactJS", image: <Reactjs width="24px" height="24px" /> },
+  { name: "NodeJS", image: <Nodejs width="24px" height="24px" /> },
+  { name: "Express", image: <Express width="24px" height="24px" /> },
+  { name: "PostgreSQL", image: <Postgres width="24px" height="24px" /> },
+  { name: "Git", image: <Git width="24px" height="24px" /> },
+  { name: "Figma", image: <Figma width="24px" height="24px" /> },
+];
