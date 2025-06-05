05c9e106a81da4b6b8fa16ad3a0c6c650ac64f82
JobertDev27 <jobertdev27@gmail.com>
2025-06-05 18:09:16 +0800

    feat: added projects data

--- src/components/data/projects.tsx
+++ src/components/data/projects.tsx
@@ -1,4 +1,10 @@
 export const projects = [
   // Upper most 3 will be displayed on the featured page
-  {},
+  {
+    name: "Minku",
+    desc: "A e-commerce store mockup site that is capable of storing data locally, search filtering and responsive layout.",
+    stack: ["React", "TypeScript", "Vite"],
+    live: "https://minku-ecommerce-site.vercel.app/",
+    code: "https://github.com/JobertDev27/Minku-ecommerce-site",
+  },
 ];
